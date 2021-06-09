6514ecc411b5e1f76a2e4016fe687f30bf82ff66
Tim <83349@roc-teraa.nl>
2021-06-09 14:26:13 +0200

    migrations

---
+++ database/migrations/2021_06_09_121117_create_user_player_teams_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserPlayerTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_player_teams', function (Blueprint $table) {
+            $table->id('id');
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('player_id');
+            $table->unsignedBigInteger('team_id');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('player_id')->references('id')->on('players')->onDelete('cascade');
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_player_teams');
+    }
+}
