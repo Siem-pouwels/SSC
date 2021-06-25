222283fce9e56a03256f41594ea60c6fb146bf75
Siem <84181@roc-teraa.nl>
2021-06-25 20:55:14 +0200

    hier

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Player;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PlayerController extends Controller
 {
     public function getPlayers(){
-        $player = Player::where('name', 'Lionel Messi')->get('name');
-        return json_encode($player);
+        $user = Auth::user();
+        $players = Player::where('user_id', '=', $user->id)->get();
+        return response()->json($players);
     }
 }
