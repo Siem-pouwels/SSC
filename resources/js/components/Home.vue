454e107920a0084f73fd917365856696386d4b8f
Yannick <82997@roc-teraa.nl>
2021-06-29 20:02:47 +0200

    caroussel

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,18 +1,11 @@
 <template>
     <div>
-        <img :src="'../storage/player_faces/card-design.png'">
-        <!-- <div id="container">
+        <div id="container">
         <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
     
         <div class="carousel-inner">
           <div class="item active"> 
-            <img src="../storage/static/packs.jpg" alt="Packstore" style="width:100%;">
+            <img :src="'../storage/player_faces/ssc_packs.jpg'" alt="Packstore" style="width:100%;">
             <div class="carousel-caption">
               <h3>Packstore</h3>
               <p>Choose a pack to get players!</p>
@@ -20,14 +13,14 @@
           </div>
     
           <div class="item">
-            <img src="../storage/static/squadbuilder.jpg" alt="Squad" style="width:100%;">
+            <img :src="'../storage/player_faces/ssc_squad.jpg'" alt="Squad" style="width:100%;">
             <div class="carousel-caption">
               <h3>Squad</h3>
               <p>Build the team of your dreams!</p>
             </div>
           </div>
           <div class="item">
-            <img src="../storage/static/match.jpg" alt="Match" style="width:100%;">
+            <img :src="'../storage/player_faces/ssc_match.jpg'" alt="Match" style="width:100%;">
             <div class="carousel-caption">
               <h3>Match</h3>
               <p>Take on other squad in a match!</p>
@@ -45,7 +38,7 @@
           <span class="sr-only">Next</span>
         </a>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 <script>
