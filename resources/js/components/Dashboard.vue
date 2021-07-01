51e2ed2243114677d09236b3d80816469f902616
Stijn Leesberg <84098@roc-teraa.nl>
2021-07-01 09:36:22 +0200

    dashboard style

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -1,14 +1,90 @@
 <template>
-    <div>
+    <div class="dashboard">
+        <img :src="'../images/account.jpg'" class="accountimage">
+        <div class="dashboard-text">
         Dashboard <br>
         <div v-if="user">
         Name: {{user.name}} <br>
         Email: {{user.email}}<br><br>
-        <button @click.prevent="logout">Logout</button>
+        <button @click.prevent="logout" class="glow-on-hover">Logout</button>
         </div>
-
+      </div>
     </div>
 </template>
+<style>
+    .dashboard {
+        margin-top: 80px;
+        font-size: 40px;
+        font-family: Dishonorable Mention;
+    }
+    .accountimage {
+        margin-top: 10px;
+        width: 200px;
+        height: 200px;
+    }
+    .dashboard-text {
+        font-family: Dishonorable Mention;
+    }
+    .glow-on-hover {
+    width: 220px;
+    height: 80px;
+    border: none;
+    outline: none;
+    color: #fff;
+    background: #111;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    border-radius: 10px;
+}
+
+.glow-on-hover:before {
+    content: '';
+    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+    position: absolute;
+    top: -2px;
+    left:-2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 0;
+    transition: opacity .3s ease-in-out;
+    border-radius: 10px;
+}
+
+.glow-on-hover:active {
+    color: #000
+}
+
+.glow-on-hover:active:after {
+    background: transparent;
+}
+
+.glow-on-hover:hover:before {
+    opacity: 1;
+}
+
+.glow-on-hover:after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+}
+
+@keyframes glowing {
+    0% { background-position: 0 0; }
+    50% { background-position: 400% 0; }
+    100% { background-position: 0 0; }
+}
+</style>
 <script>
 export default {
     data(){
